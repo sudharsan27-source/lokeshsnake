fedce0fcff32b25f1b38e1c116180c8eb5c92f37
Sudharsan27 <vksudharsann@gmail.com>
2024-03-07 12:51:08 +0530

    img added.

---
+++ snake.js
@@ -0,0 +1,88 @@
+const data = [
+    {
+      id: 1,
+      name: "Fire Boltt Ninja 2",
+      img: "https://m.media-amazon.com/images/I/617eiZeFtNL._SL1500_.jpg",
+      amt: 1599,
+      seller: "Boltt Store",
+      catagory: "Watch",
+    },
+  
+    {
+      id: 2,
+      name: "Noise Pulse Go",
+      img: "https://m.media-amazon.com/images/I/61akt30bJsL._SL1500_.jpg",
+      amt: 1300,
+      seller: "Noise Store",
+      catagory: "Watch",
+    },
+  
+    {
+      id: 3,
+      name: "boAt Xtend Pro",
+      img: "https://m.media-amazon.com/images/I/61ZuL8CUigL._SL1500_.jpg",
+      amt: 2799,
+      seller: "Rajesh Watchs",
+      catagory: "Watch",
+    },
+    {
+      id: 4,
+      name: "Lenovo Tab M8",
+      img: "https://m.media-amazon.com/images/I/71SvqTFPXJL._SL1500_.jpg",
+      amt: 9270,
+      seller: "Stonehenge Retail",
+      catagory: "Tablets",
+    },
+    {
+      id: 5,
+      name: "Honor PAD X8",
+      img: "https://m.media-amazon.com/images/I/710G-VKcgtL._SL1500_.jpg",
+      amt: 12999,
+      seller: "Honor india",
+      catagory: "Tablets",
+    },
+  
+    {
+      id: 6,
+      name: "IKALL N9 ",
+      img: "https://m.media-amazon.com/images/I/7185GL6hPlL._SL1500_.jpg",
+      amt: 3999,
+      seller: "IKALL Store",
+      catagory: "Tablets",
+    },
+  
+    {
+      id: 7,
+      name: "Oppo Pad Air",
+      img: "https://m.media-amazon.com/images/I/513FD4w8hGL._SL1500_.jpg",
+      amt: 15999,
+      seller: "Oppo Store",
+      catagory: "Tablets",
+    },
+    {
+      id: 8,
+      name: "Acer EK220Q",
+      img: "https://m.media-amazon.com/images/I/8150iUXkc5L._SL1500_.jpg",
+      amt: 6249,
+      seller: "Accer Store",
+      catagory: "Monitors",
+    },
+    {
+      id: 9,
+      name: "Samsung 24",
+      img: "https://m.media-amazon.com/images/I/81TjRLHaz1L._SL1500_.jpg",
+      amt: 9799,
+      seller: "Samsung Store",
+      catagory: "Monitors",
+    },
+  
+    {
+      id: 10,
+      name: "ZEBRONICS AC32FHD ",
+      img: "https://m.media-amazon.com/images/I/813Y1TIZwfL._SL1500_.jpg",
+      amt: 12799,
+      seller: "ZEBRONICS Store",
+      catagory: "Monitors",
+    },
+  ];
+  console.log(data);
