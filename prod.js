773d293cef2fa05342b8b5900cc655fd5bf11d32
Sudharsan27 <vksudharsann@gmail.com>
2024-03-13 17:45:59 +0530

    js changes

---
+++ prod.js
@@ -0,0 +1,10 @@
+<div class="product">
+<div class="img">
+   <img src="https://m.media-amazon.com/images/I/7185GL6hPlL._SL1500_.jpg" alt="Product Name" />
+</div>
+<div class="product-details">
+   <span class="name">product Name</span>
+   <span class="amt">RS.5000</span>
+   <span class="sellar">ABC Company</span>
+</div>
+</div>
